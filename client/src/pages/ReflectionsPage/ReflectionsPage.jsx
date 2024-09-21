371cd79b4f8c42d674935b659ec54d868b701079
Michelle Rafols <elle.raf18@gmail.com>
2024-09-21 18:27:26 -0400

    amend reflections form to include title

---
+++ client/src/pages/ReflectionsPage/ReflectionsPage.jsx
@@ -0,0 +1,43 @@
+import './ReflectionsPage.scss';
+
+export default function ReflectionsPage() {
+    return (
+        // <div className="reflections-page">
+        //     <h1 className="reflections-page__title">Reflections</h1>
+        //     <p className="reflections-page__description">
+        //         Write and view your reflections on your daily activities and Bible verses.
+        //     </p>
+        //     <textarea
+        //         className="reflections-page__textarea"
+        //         placeholder="Write your reflection here..."
+        //     />
+        //     <button className="reflections-page__submit-btn">Submit Reflection</button>
+        // </div>
+        <section className="reflections-page">
+            <form id="reflectionsForm" className="reflections__form">
+                <h1 className="reflections-page__title">Reflections</h1>
+                <p className="reflections-page__description">
+                    Write and view your reflections on your daily activities and Bible verses.
+                </p>
+                <div className="reflections-page__title-container">
+                    <input
+                        type="text"
+                        name="reflectionsTitle"
+                        className="reflections-page__input-title"
+                        placeholder="Add a title to your reflection"
+                        required
+                    />
+                </div>
+                <div className="reflections-page__textarea-container">
+                    <textarea
+                        className="reflections-page__textarea"
+                        placeholder="Write your reflection here..."
+                    />
+                </div>
+                <button className="reflections-page__submit-btn">
+                    Submit Reflection
+                </button>
+            </form>
+        </section>
+    );
+}
