36f936da63a30ec0bb850f19728dff69144a65ec
Michelle Rafols <elle.raf18@gmail.com>
2024-09-21 22:10:57 -0400

    cleanup code

--- client/src/pages/ReflectionsPage/ReflectionsPage.jsx
+++ client/src/pages/ReflectionsPage/ReflectionsPage.jsx
@@ -2,17 +2,6 @@ import './ReflectionsPage.scss';
 
 export default function ReflectionsPage() {
     return (
-        // <div className="reflections-page">
-        //     <h1 className="reflections-page__title">Reflections</h1>
-        //     <p className="reflections-page__description">
-        //         Write and view your reflections on your daily activities and Bible verses.
-        //     </p>
-        //     <textarea
-        //         className="reflections-page__textarea"
-        //         placeholder="Write your reflection here..."
-        //     />
-        //     <button className="reflections-page__submit-btn">Submit Reflection</button>
-        // </div>
         <section className="reflections-page">
             <form id="reflectionsForm" className="reflections__form">
                 <h1 className="reflections-page__title">Reflections</h1>
