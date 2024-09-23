376a251d53eb7e916c04b5cc94b57aa1ad8a1cae
Michelle Rafols <elle.raf18@gmail.com>
2024-09-22 20:51:48 -0400

    fix navbar styling

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,24 +2,23 @@ import './Navbar.scss';
 import logo from '../../assets/logo/logo.png';
 import { Link, NavLink } from 'react-router-dom';
 
-
 export default function Navbar() {
     return (
         <section className="navbar">
             <ul className="navbar__list">
                 <li className="navbar__item">
-                    <Link to='/'>
+                    <Link to='/' className="navbar__logo-link">
                         <img className="navbar__logo" src={logo} alt="Sprout logo" />
                     </Link>
                 </li>
                 <ul className="navbar__sublist">
                     <li className="navbar__item">
-                        <NavLink to='/'>
+                        <NavLink to='/' className="navbar__link" activeClassName="navbar__link--active">
                             Home
                         </NavLink>
                     </li>
                     <li className="navbar__item">
-                        <NavLink to='/reflections/list'>
+                        <NavLink to='/reflections/list' className="navbar__link" activeClassName="navbar__link--active">
                             Reflections
                         </NavLink>
                     </li>
