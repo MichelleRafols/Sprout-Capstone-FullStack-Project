682a5a0a9ed757ef9e52ff6a59e8120d49cc9999
Michelle Rafols <elle.raf18@gmail.com>
2024-09-22 21:25:49 -0400

    fix navbar jsx

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -7,18 +7,24 @@ export default function Navbar() {
         <section className="navbar">
             <ul className="navbar__list">
                 <li className="navbar__item">
-                    <Link to='/' className="navbar__logo-link">
+                    <Link to='/' className="navbar__link-logo">
                         <img className="navbar__logo" src={logo} alt="Sprout logo" />
                     </Link>
                 </li>
                 <ul className="navbar__sublist">
                     <li className="navbar__item">
-                        <NavLink to='/' className="navbar__link" activeClassName="navbar__link--active">
+                        <NavLink 
+                            to='/' 
+                            className={({ isActive }) => isActive ? 'navbar__link navbar__link--active' : 'navbar__link'}
+                        >
                             Home
                         </NavLink>
                     </li>
                     <li className="navbar__item">
-                        <NavLink to='/reflections/list' className="navbar__link" activeClassName="navbar__link--active">
+                        <NavLink 
+                            to='/reflections/list' 
+                            className={({ isActive }) => isActive ? 'navbar__link navbar__link--active' : 'navbar__link'}
+                        >
                             Reflections
                         </NavLink>
                     </li>
