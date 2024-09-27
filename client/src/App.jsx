c8a88104979a3283e63538fac84b7690702a1c78
Michelle Rafols <elle.raf18@gmail.com>
2024-09-27 11:00:21 -0400

    add footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import ReflectionsPage from './pages/ReflectionsPage/ReflectionsPage';
 import ReflectionsListPage from './pages/ReflectionsListPage/ReflectionsListPage';
 import EditReflectionPage from './pages/EditReflectionsPage/EditReflectionsPage';
 import EnergyDetailsPage from './pages/EnergyDetailsPage/EnergyDetailsPage';
+import Footer from './components/Footer/Footer';
 
 function App() {
 
@@ -20,11 +21,12 @@ function App() {
         <Route path="/energy-details/:energyLevel" element={<EnergyDetailsPage />} />
         <Route path='/levels/:id/indoor-activities' element={<IndoorActivitiesPage />} />
         <Route path='/levels/:id/outdoor-activities' element={<OutdoorActivitiesPage />} />
-        <Route path='/reflections' element={<ReflectionsPage/>}/>
+        <Route path='/reflections' element={<ReflectionsPage />} />
         <Route path="/reflections/list" element={<ReflectionsListPage />} />
         <Route path="/reflections/:id/edit" element={<EditReflectionPage />} />
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
